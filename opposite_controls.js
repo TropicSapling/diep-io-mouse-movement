96e5688b61769d57d02637927df92c486cd63084
Tropic <tropical.productions.scratch@gmail.com>
2017-03-10 13:12:53 +0100

    Create opposite_controls.js

---
+++ opposite_controls.js
@@ -0,0 +1,35 @@
+// PASTE THIS WHOLE FILE INTO CONSOLE TO ACTIVATE
+
+var mouse_x;
+var mouse_y;
+onmousemove = function(e){mouse_x = e.clientX; mouse_y = e.clientY}
+
+setInterval(function() {
+	input.keyUp(65);
+	input.keyUp(68);
+	input.keyUp(83);
+	input.keyUp(87);
+	
+	var distance_x = mouse_x - window.innerWidth / 2;
+	var distance_y = 0 - (mouse_y - window.innerHeight / 2);
+	
+	var x_change = Math.sin(Math.atan2(distance_x, distance_y));
+	var y_change = Math.cos(Math.atan2(distance_x, distance_y));
+	
+	var x_speed_proto = Math.abs(distance_x) / (window.innerWidth / 4);
+	var y_speed_proto = Math.abs(distance_y) / (window.innerHeight / 4);
+	var x_speed = x_speed_proto < 1 ? x_speed_proto : 1;
+	var y_speed = y_speed_proto < 1 ? y_speed_proto : 1;
+
+	if(x_change * x_speed < 0 - Math.random()) {
+		input.keyDown(68);
+    } else if(x_change * x_speed > Math.random()) {
+		input.keyDown(65);
+    }
+	
+	if(y_change * y_speed < 0 - Math.random()) {
+		input.keyDown(87);
+    } else if(y_change * y_speed > Math.random()) {
+		input.keyDown(83);
+    }
+}, 10);
